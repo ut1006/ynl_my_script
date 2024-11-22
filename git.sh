8d42b0e5c487fd22f7268a0426a13bf5d2bf052f
A.KMD <akkmd100610061006@gmail.com>
2024-11-23 00:49:56 +0900

    11/22 add auto hsv

--- git.sh
+++ git.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # コミットメッセージの引数がなければエラーメッセージを表示して終了
-if [ -z "$1" ]; then
-  echo "Usage: $0 \"commit message\""
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 commit message"
   exit 1
 fi
 
+# 引数をスペース区切りで結合して、1つのメッセージにする
+commit_message="$*"
+
 # 一連のGitコマンドを実行
 git add .
-git commit -m "$1"
+git commit -m "$commit_message"
 git push origin master
